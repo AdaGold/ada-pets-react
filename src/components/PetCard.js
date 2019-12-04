9ba4bb4bdb72cb66bc8999380cd87362282756c3
CheezItMan <chris@adadevelopersacademy.org>
2019-12-03 16:46:49 -0800

    individual petcard

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -8,28 +8,30 @@ import speciesEmoji from '../speciesEmoji';
 
 
 const PetCard = (props) => {
-  const { id, name, species, about, location } = props;
+  const { id, name, species, about, location, selectPetCallback, deletePetCallback } = props;
   return (
     <div className="card pet-card">
 
       <section className="pet-card--header">
 
-      { speciesEmoji(species) } {id} - {name} 
-        <button 
+        {speciesEmoji(species)} {id} - {name}
+        <button
           className="btn btn-primary pet-card--select-pet-btn"
-          >
-            Select
+          onClick={() => { selectPetCallback(id) }}
+        >
+          Select
         </button>
-        <button 
-          type="button" 
-          className="btn btn-danger pet-card--close-btn" 
+        <button
+          type="button"
+          className="btn btn-danger pet-card--close-btn"
           aria-label="Close"
+          onClick={() => { deletePetCallback(id) }}
         >
           Close
         </button>
       </section>
       <section className="pet-card--body">
-        { about.length > 128 ? `${about.substring(0, 128)}...` : about}
+        {about.length > 128 ? `${ about.substring(0, 128) }...` : about}
       </section>
       <section className="pet-card--footer text-muted">
         {location}
@@ -37,13 +39,15 @@ const PetCard = (props) => {
     </div>
   );
 };
-    
+
 PetCard.propTypes = {
   id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired, 
-  species: PropTypes.string.isRequired, 
-  about: PropTypes.string, 
+  name: PropTypes.string.isRequired,
+  species: PropTypes.string.isRequired,
+  about: PropTypes.string,
   location: PropTypes.string,
+  selectPetCallback: PropTypes.func,
+  deletePetCallback: PropTypes.func,
 }
-    
+
 export default PetCard;
