379993a8885ff45aa3f8a082b18a1344383c6b32
CheezItMan <chris@adadevelopersacademy.org>
2019-12-03 16:47:05 -0800

    updated PetList

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -4,19 +4,30 @@ import PetCard from './PetCard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const getPetCards = (pets, selectPetCallback, deletePetCallback) => {
+  return pets.map((pet) => {
+    return <PetCard
+      key={pet.id}
+      {...pet}
+      selectPetCallback={selectPetCallback}
+      deletePetCallback={deletePetCallback}
+    />
+  });
+}
 
-const PetList = (props) => {
-
+const PetList = ({ pets, selectPetCallback, deletePetCallback }) => {
 
   return (
     <div className="card-group">
+      {getPetCards(pets, selectPetCallback, deletePetCallback)}
     </div>
   )
 }
 
 PetList.propTypes = {
   pets: PropTypes.array.isRequired,
-  onSelectPet: PropTypes.func,
+  selectPetCallback: PropTypes.func,
+  deletePetCallback: PropTypes.func,
 };
 
 export default PetList;
