0d1706a93b2f3acd72fb608e8dbee2b3bcbde451
CheezItMan <chris@adadevelopersacademy.org>
2019-12-03 16:50:02 -0800

    SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,18 +9,41 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      searchBar: '',
+    }
   }
 
-  render() {
+  onInputChange = (event) => {
+    const searchBar = event.target.value;
+
+    this.setState({
+      searchBar,
+    });
+
+    this.props.searchChangeCallback(searchBar);
+  }
+
+  render () {
     return (
       <section>
+        <div>
+          <label className="search-bar--label" htmlFor="searchBar">Search</label>
+        </div>
+        <input
+          onChange={this.onInputChange}
+          value={this.state.searchBar}
+          name="searchBar"
+          id="searchBar"
+          className="search-bar"
+        />
       </section>
     );
   }
 };
 
 SearchBar.propTypes = {
-  
+  searchChangeCallback: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
