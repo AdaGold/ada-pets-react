b947fcc185a5b9e9dedf516b8f753747d590a388
CheezItMan <chris@adadevelopersacademy.org>
2019-12-06 10:55:42 -0800

    lift state from SearchBar to App

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,41 +4,23 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SearchBar.css';
 
-class SearchBar extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchValue: '',
-    };
-  }
-
-  onSearchChange = (event) => {
-    this.setState({
-      searchValue: event.target.value,
-    });
-
-    this.props.onSearchChange(event.target.value);
-  }
-
-  render() {
-    return (
-      <section>
-        <input 
-          onChange={this.onSearchChange}
-          value={this.state.searchValue}
-          name="search-bar"
-          className="search-bar"
-          placeholder="Filter Pets"
-        />
-      </section>
-    );
-  }
+const SearchBar = ({ searchTerm, onTermChangeCallback }) => {
+  return (
+    <section>
+      <input
+        onChange={(event) => { onTermChangeCallback(event.target.value); }}
+        value={searchTerm}
+        name="search-bar"
+        className="search-bar"
+        placeholder="Filter Pets"
+      />
+    </section>
+  );
 };
 
 SearchBar.propTypes = {
-  onSearchChange: PropTypes.func,
+  onTermChangeCallback: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
