6928778d3523a78cb8508a1cd64af4050d03e4e6
CheezItMan <chris@adadevelopersacademy.org>
2019-12-06 11:30:30 -0800

    Elevate SearchBar's state to App

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,46 +4,26 @@ import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './SearchBar.css';
 
-class SearchBar extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      searchBar: '',
-    }
-  }
-
-  onInputChange = (event) => {
-    const searchBar = event.target.value;
-
-    this.setState({
-      searchBar,
-    });
-
-    this.props.searchChangeCallback(searchBar);
-  }
-
-  render () {
-    return (
-      <section>
-        <div>
-          <label className="search-bar--label" htmlFor="searchBar">Search</label>
-        </div>
-        <input
-          onChange={this.onInputChange}
-          value={this.state.searchBar}
-          name="searchBar"
-          id="searchBar"
-          className="search-bar"
-        />
-      </section>
-    );
-  }
+const SearchBar = ({ searchTerm, searchChangeCallback }) => {
+  return (
+    <section>
+      <div>
+        <label className="search-bar--label" htmlFor="searchBar">Search</label>
+      </div>
+      <input
+        onChange={(event) => { searchChangeCallback(event.target.value) }}
+        value={searchTerm}
+        name="searchBar"
+        id="searchBar"
+        className="search-bar"
+      />
+    </section>
+  );
 };
 
 SearchBar.propTypes = {
   searchChangeCallback: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
 };
 
 export default SearchBar;
