9831dd0d46b617b292c7a8f7526f2c85a3fff1e8
CheezItMan <chris@adadevelopersacademy.org>
2019-12-06 09:45:05 -0800

    NewPetForm snapshot test

---
+++ src/components/test/NewPetForm.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react'
+import NewPetForm from '../NewPetForm';
+
+describe('NewPetForm', () => {
+  test('that it matches the existing snapshot', () => {
+    // Arrange-Act
+    const { asFragment } = render(<NewPetForm
+      addPetCallback={() => { }}
+    />);
+
+    // Assert
+    expect(asFragment()).toMatchSnapshot();
+    // unmount the rendered content
+    cleanup();
+  });
+});
