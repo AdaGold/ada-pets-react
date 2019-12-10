a009956794bce11b1cdb8d66827d34831bcaf1ad
CheezItMan <chris@adadevelopersacademy.org>
2019-12-10 10:26:19 -0800

    petDetails to use destructuring

--- src/components/PetDetails.js
+++ src/components/PetDetails.js
@@ -6,13 +6,13 @@ import speciesEmoji from '../speciesEmoji';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PetDetails.css';
 
-const PetDetails = (props) => {
-  const { name, location, about, species, images } = props.currentPet;
+const PetDetails = ({ currentPet }) => {
+  const { name, location, about, species, images } = currentPet;
   return (
     <section className="pet-details">
       <section className="pet-details--header">
         <h2 className="petdetails--name">{speciesEmoji(species)} {name} {speciesEmoji(species)}</h2>
-        <img src={images[0]} alt={`${name}`} className="pet-details--image" />
+        <img src={images[0]} alt={`${ name }`} className="pet-details--image" />
       </section>
       <article>
         <h3>About {name}</h3>
