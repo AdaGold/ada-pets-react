ff58cde7500676de422f454d7d44fb407ee42248
Jared Maddox <jared@adadevelopersacademy.org>
2019-12-10 16:13:01 -0800

    changes after testing lesson

--- src/components/test/PetCard.test.js
+++ src/components/test/PetCard.test.js
@@ -4,15 +4,16 @@ import { render, cleanup } from '@testing-library/react'
 import PetCard from '../PetCard';
 
 
-let doNothing = () => {};
+const doNothing = () => {};
 const selectPet = jest.fn();
-let petCard = <PetCard
+const deletePet = jest.fn();
+const petCard = <PetCard
         id= {1}
         name= "Denali"
-        species= "Golden Retriever"
+        species= "dog"
         about="Most empathetic dog ever"
         location="Seattle, WA"
-        deletePetCallback= {doNothing}
+        deletePetCallback= {deletePet}
         selectPetCallback= {selectPet}
       />;
 
@@ -25,12 +26,9 @@ describe('PetCard', () => {
 
     // Assert
     expect(result.asFragment()).toMatchSnapshot();
-    cleanup();
   });
-});
 
-describe('PetCard', () => {
-  test('that it matches the existing snapshot', () => {
+  test('that it shows the dogs name', () => {
     // Arrange-Act
     const result = render(
       petCard
@@ -38,22 +36,53 @@ describe('PetCard', () => {
 
     // Assert
     expect(result.getByText(/Denali/)).toBeDefined();
-    cleanup();
+    expect(result.getByText(/Seattle, WA/)).toBeDefined();
+    expect(result.getByText(/🐕/)).toBeDefined();
   });
-});
 
-test('The "selectPetCallback" function is called when the `select` button is clicked on', () => {
-  // Render a PetCard
-  const result = render(petCard);
-  
+  test('The "selectPetCallback" function is called when the `select` button is clicked on', () => {
+    // Render a PetCard
+    const selectPet = jest.fn();
+    const petCard = <PetCard
+            id= {1}
+            name= "Denali"
+            species= "dog"
+            about="Most empathetic dog ever"
+            location="Seattle, WA"
+            deletePetCallback= {doNothing}
+            selectPetCallback= {selectPet}
+          />;
 
-  // Act
-  // Find the "Select" button
-  const selectButton = result.getByText(/Select/);
-  // Trigger a 'click' event
-  selectButton.click();
+    const result = render(petCard);
+    
 
-  // Assert
-  // Verify that the callback function was called.
-  expect(selectPet).toHaveBeenCalled();
+    // Act
+    // Find the "Select" button
+    const selectButton = result.getByText(/Select/);
+    // Trigger a 'click' event
+    selectButton.click();
+
+    // Assert
+    // Verify that the callback function was called.
+    expect(selectPet).toHaveBeenCalled();
+  });
+
+  test('The "selectPetCallback" function is called when the `select` button is clicked on', () => {
+    // Render a PetCard
+
+    const result = render(petCard);
+    
+
+    // Act
+    // Find the "Select" button
+    const deleteButton = result.getByText(/Close/);
+    // Trigger a 'click' event
+    deleteButton.click();
+
+    // Assert
+    // Verify that the callback function was called.
+    expect(deletePet).toHaveBeenCalled();
+  });
+  afterEach(cleanup);
+
 });
